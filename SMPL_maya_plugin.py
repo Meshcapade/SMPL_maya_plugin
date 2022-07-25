549718a9b9216f2f8c236c807bbbaf2efff0c149
Naureen Mahmood <naureen@meshcapade.com>
2022-07-25 19:02:29 +0200

    update README

--- SMPL_maya_plugin.py
+++ SMPL_maya_plugin.py
@@ -7,33 +7,18 @@ For comments or questions, please email us at: support@meshcapade.com
 
 
 Current versions supported:
---------------------------
-Mac OSX: Maya 2014+
-Windows: Maya 2014+
-
-
-Dependencies:
-------------
-Numpy is required for running this script. Numpy is a python module that
-can be installed following the instructions given here:
-http://docs.scipy.org/doc/numpy/user/install.html
-
-or here:
-http://blog.animateshmanimate.com/post/115538511578/python-numpy-and-maya-osx-and-windows
-
-Please make sure you have numpy installed on your computer and accessible through Maya's python.
-We are working towards removing this dependency. 
+---------------------------
+Maya 2023+
 
 
 About the Script:
------------------
-The script displays a UI to apply SMPL's shape and pose blendshapes and to adjust the skeleton to new body shapes.
-Load this plugin into Maya. It will create a window with 3 options:
+----------------
+The script displays a UI to apply the pose-corrective blendshapes for SMPL, SMPLH, SMPLX and STAR models in Maya. Load this plugin into Maya. It will create a window with 3 options:
 
 1- Apply Pose Blend Shapes to Current Frame: 
     If you repose the model in Maya, then click this to 
     compute and apply the pose blend shapes in the current frame. 
-    You can als ochoose whether or not to set the keyframes for the 
+    You can also choose whether or not to set the keyframes for the 
     pose blendshapes. Check the 'Reset Keyframes' checkbox if you 
     would like to lock blendShape values at given frame by setting 
     a keyframe. 
@@ -44,8 +29,11 @@ Load this plugin into Maya. It will create a window with 3 options:
     'Reset Keyframes' checkbox if you would like to lock blendShape 
     values at given frame range by setting a keyframe at each frame in the 
     given range.
+    
+3- Make Pose Blend Shapes fire interactively: 
+    Click this button to turn on automatic pose-correctives for any SMPL(-/H/X) or STAR rigged mesh. Once this is set to ON, then any time you repose the SMPL model, the pose-correctives will automatically be applied to the mesh.
 
-Always make sure to cilck on the mesh in the 3D view to select it before 
+Always make sure to click on the mesh in the 3D view to select it before 
 using any of the functions in the plugin. Select only the mesh of the model 
 you want to update and then click the appropriate button on the UI.
 
